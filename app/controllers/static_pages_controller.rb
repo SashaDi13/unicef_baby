6f97e1f525627d417b848e9bdec9143c35fb5303
Diana <dianaporogy@gmail.com>
2021-10-09 14:40:38 +0300

    Create project unicef_baby and generate first controller StaticPages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def main
+  end
+
+  def about
+  end
+
+  def advises
+  end
+
+  def contact
+  end
+end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /main" do
+    it "returns http success" do
+      get "/static_pages/main"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /about" do
+    it "returns http success" do
+      get "/static_pages/about"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /advises" do
+    it "returns http success" do
+      get "/static_pages/advises"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /contact" do
+    it "returns http success" do
+      get "/static_pages/contact"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
