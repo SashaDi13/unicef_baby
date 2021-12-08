55263fe9616f403d624f7f8c86d80d7d982e8c0f
Diana <dianaporogy@gmail.com>
2021-12-08 22:25:43 +0200

    Must delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ FactoryBot.create(:category, :add)
 FactoryBot.create(:category, :emergency)
 
 FactoryBot.create(:admin)
+FactoryBot.create(:admin, email: "olecsandra.di@gmail.com") # DELETE BEFORE ADD TO GIT!!!!!!!!!
 
 Category.all.each do |category|
   FactoryBot.create_list(:article, 8, category: category)
