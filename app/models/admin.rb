918b0066936ead3f19490e797efe616270931953
Diana <dianaporogy@gmail.com>
2021-12-30 16:13:19 +0200

    Add registration with google

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,11 +2,21 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   validates :login, presence: true
+  attr_accessor :skip_password_validation
 
   def self.from_omniauth(auth)
-    google_admin = Admin.find_or_initialize_by(email: auth.info.email)
+    data = auth.info
+    google_admin = Admin.where(email: data['email']).first
+
+    unless google_admin
+        google_admin = Admin.create(login: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
     google_admin
   end
 
