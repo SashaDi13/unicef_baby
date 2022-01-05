b97cc85396db96694fc631bf39ea5c6fd86f51ef
Diana <dianaporogy@gmail.com>
2022-01-05 14:12:32 +0200

    Add slack notifier

---
+++ config/initializers/slack.rb
@@ -0,0 +1,3 @@
+NOTIFIER = Slack::Notifier.new "https://hooks.slack.com/services/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",
+                                channel: "#project",
+                                username: "notifier"
