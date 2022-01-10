390e29a55b6de362df0e270611d50bca631b3a7b
Diana <dianaporogy@gmail.com>
2022-01-10 14:31:26 +0200

    modify test

--- spec/jobs/send_slack_notification_job_spec.rb
+++ spec/jobs/send_slack_notification_job_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe SendSlackNotificationJob, type: :job do
   describe "#perform" do
-    let(:admin) { "exemple@dot.net" }
-    let(:category) { create(:category) }
-    let(:article) { create(:article, category_id: category.id,
-                                     published_at: Time.zone.now - 10.minutes) }
     let(:text) { Slack::Notifier.new 'http://example.com/webhook', username: 'Max',
                                                                   channel: '#test',
                                                                   icon_emoji: ":page_facing_up:"}
