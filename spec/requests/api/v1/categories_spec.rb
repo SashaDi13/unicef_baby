a3086c9f72f5b19c089d5e2e268f7054b1bfbfe8
Diana <dianaporogy@gmail.com>
2022-01-21 19:10:46 +0200

    fix categories_spec.rb api test

--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -1,22 +1,43 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/categories/:id', type: :request do
-  let(:category) { create(:category) }
-  let(:articles) { create_list(:article, 4, category_id: category.id)}
-  # let(:response_json) { JSON.parse(response.body) }
+  describe 'Category API' do
+    category = Category.first
+    let(:id) { category.id }
 
-  describe 'GET #show' do
-    before :each do
-      get api_v1_category_path(category)
-    end
-    it 'returns http success' do
-      response_json = JSON.parse(response.body)
+    path "/api/v1/categories/{id}" do
+      get 'Lists category articles' do
+        tags 'category'
+        consumes 'application/json'
+        produces 'application/json'
 
-      expect(response_json['category'].size).to eq(4)
-    end
+        let(:articles) { create_list(:article, 4, category_id: category.id)}
+        let(:response_json) { JSON.parse(response.body) }
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(:success)
+        response '200', 'lists articles' do
+          it 'Return 4 articles' do
+            expect(response_json['category'].count).to eq(4)
+          end
+
+          parameter name: :id, in: :path, type: :integer
+
+          schema type: :object,
+           properties: {
+             activities: {
+               type: :array,
+               items: {
+                 properties: {
+                   title: { type: :string },
+                   published_at: { type: :datetime },
+                   image: { type: :string }
+                 },
+                 required: %i[title, published_at, image]
+               }
+             }
+           }
+          run_test!
+        end
+      end
     end
   end
 end
