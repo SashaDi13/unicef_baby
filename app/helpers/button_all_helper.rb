f089064d172e6697080a5462b0f014f8cd6abcc6
Diana <dianaporogy@gmail.com>
2022-02-25 17:25:25 +0200

    Save session search

--- app/helpers/button_all_helper.rb
+++ app/helpers/button_all_helper.rb
@@ -6,4 +6,12 @@ module ButtonAllHelper
       link_to "ПЕРЕГЛЯНУТИ ВСІ СТАТТІ", articles_path, class: "btn btn-primary btn-lg btn-all-art px-5 py-3 mt-5"
     end
   end
+
+  def category_link(category)
+    if session["search_#{category.id}"]
+      session["search_#{category.id}"]
+    else
+      category_path(category)
+    end
+  end
 end
